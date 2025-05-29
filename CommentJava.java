0052f562b14c598f4ecc32a5782990318830d426
ryujikang <dbwlrkd2000@naver.com>
2025-05-29 14:18:16 +0900

    section2까지 수강 완료

---
+++ CommentJava.java
@@ -0,0 +1,22 @@
+public class CommentJava {
+
+    // psvm 이라고 작성하면 main 메소드가 자동으로 만들어짐
+    public static void main(String[] args) {
+
+        // sout 이라고 작성하면 System.out.println() 메소드가 자동으로 만들어짐
+        System.out.println("hello java1");
+
+        System.out.println("hello java2");
+
+        // 주석처리 됬으므로 실행 안됨
+        //System.out.println("hello java3");
+
+        /*
+        System.out.println("hello java4");
+        System.out.println("hello java5");
+        */
+
+        // : Ctrl + '/'
+        /* Ctrl + shift + '/' */
+    }
+}
