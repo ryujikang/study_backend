653b0dae2045ce3087327d263725cb187aacf8f8
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-06-26 23:41:37 +0900

    Title: 숫자 피라미드, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250626/숫자 피라미드/number-pyramid.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n =Integer.parseInt(br.readLine());
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                bw.write(i + " ");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
