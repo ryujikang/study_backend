1f6d221b24b67d32cd841e66fab724884a4e6b2f
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-06-27 09:54:08 +0900

    Title: 숫자 2개 출력, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250627/숫자 2개 출력/print-two-numbers.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(3 + " " + 5);
+
+        bw.flush();
+        bw.close();
+    }
+}
