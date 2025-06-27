fa8473c27b5d3affec374f49ec89bb290c2880a1
ryujikang <104420236+ryujikang@users.noreply.github.com>
2025-06-27 09:52:12 +0900

    Title: 2줄 출력, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250627/2줄 출력/print-two-lines.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello\nWorld");
+
+        bw.flush();
+        bw.close();
+    }
+}
